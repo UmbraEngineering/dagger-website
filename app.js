c1510533c773731e22b9c746e0a1bd73c25c7b4a
James Brumond <kbjr14@gmail.com>
2014-04-14 19:39:03 -0700

    added handlebars rendering with environment support

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
-var fs       = require('fs');
 var express  = require('express');
+var render   = require('./lib/render');
 
 var app = express();
 
@@ -14,22 +14,33 @@ app.use(express.static(__dirname + '/public', {
 
 // All of these pages respond with index.html
 app.get('/',             loadIndex);
-app.get('/welcome',      loadIndex);
+app.get('/intro',        loadIndex);
 app.get('/get-started',  loadIndex);
 app.get('/example',      loadIndex);
-app.get('/docs/:page?',  loadIndex)
+app.get('/docs/:page?',  loadIndex);
 
-var index = __dirname + '/index.html';
+global.ENV = process.env.NODE_ENV;
+
 function loadIndex(req, res) {
-	fs.readFile(index, 'utf8', function(err, content) {
-		if (err) {
-			res.send(500, 'Could not read index.html');
-			return;
-		}
-
+	try {
+		var rendered = render({
+			env: ENV,
+			production: (ENV === 'production'),
+			development: (ENV === 'development')
+		});
 		res.contentType('text/html');
-		res.send(200, content);
-	});
+		res.send(200, rendered);
+	}
+
+	// Catch-all case for errors
+	catch (err) {
+		console.error(err.stack || err);
+		var stack = '';
+		if (env === 'development') {
+			stack = ': ' + (err.stack || err.message || err);
+		}
+		res.send(500, 'Internal server error' + stack);
+	}
 }
 
 app.listen(process.env.PORT || 5000, function() {
